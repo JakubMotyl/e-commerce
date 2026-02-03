4ff8320afe62332f8edebdfe60f7f6904627267b
Jakub Motyl <kubamotyl16@gmail.com>
2026-02-03 09:40:15 +0100

    Finish styling filters section

--- components/shop/Hero.tsx
+++ components/shop/Hero.tsx
@@ -1,15 +1,20 @@
+"use client";
+
+import { useState } from "react";
 import ShopFilters from "./ShopFilters";
 import ShopItems from "./ShopItems";
 
 export default function Hero() {
+  const [filters, setFilters] = useState([]);
   return (
     <main className="py-default">
-      <h1 className="text-4xl md:text-5xl font-semibold text-terracotta text-left w-full xl:px-16 2xl:px-20">
+      <h1 className="text-4xl md:text-5xl font-semibold text-terracotta text-left w-full xl:px-16 2xl:px-20 px-5">
         Shop
       </h1>
       <div className="w-full h-px bg-black rounded md:my-8 my-6"></div>
-      <section className="px-default">
+      <section className="px-default flex flex-col gap-5">
         <ShopFilters />
+        <ShopItems />
       </section>
     </main>
   );
--- components/shop/ShopFilters.tsx
+++ components/shop/ShopFilters.tsx
@@ -3,19 +3,19 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 
 function ShopFilters() {
   return (
-    <section className="flex items-center justify-between">
+    <section className="flex items-center justify-between text-pure-black md:text-sm text-xs font-medium">
       <div className="flex items-center gap-8">
-        <div className="flex items-center gap-0.5">
-          <GoPlus />
+        <div className="flex items-center gap-0.5 group">
+          <GoPlus className="cursor-pointer" />
           <span>Filters</span>
         </div>
         <button className="reset-btn">Reset all</button>
       </div>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 group">
         <p>Sort by:</p>
         <div className="flex items-center gap-0.5">
           <span>Default</span>
-          <MdKeyboardArrowDown />
+          <MdKeyboardArrowDown className="cursor-pointer" />
         </div>
       </div>
     </section>
