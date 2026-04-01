3afabf1ff60a7358a6970024037cc24f36fe8ac2
Jakub Motyl <kubamotyl16@gmail.com>
2026-04-01 23:15:19 +0200

    Update prisma settings for postgres

--- app/api/coupons/route.ts
+++ app/api/coupons/route.ts
@@ -1,12 +1,5 @@
-import { PrismaClient } from "@/lib/generated/prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
-
-const adapter = new PrismaBetterSqlite3({
-    url: process.env.DATABASE_URL || "file:./dev.db",
-});
-
-const prisma = new PrismaClient({ adapter });
 
 export async function POST(request: Request) {
     try {
